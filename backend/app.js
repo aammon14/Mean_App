4c9e19d6b402c57f578a31c957d700c356f4cc75
Andrew Ammon <andrewammon@Andrews-MBP.home>
2019-01-27 16:30:13 -0500

    added mongodb and delete post method

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
 
-app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://aammon:qRhQR3wHGgYBEK0l@cluster0-ynwzr.mongodb.net/node-angular?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,30 +24,32 @@ app.use((req, res, next) => {
 })
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { 
-      id: 'asdf', 
-      title: 'First server-side post', 
-      content: 'This is coming from the server'
-    },
-    { 
-      id: 'asdfg', 
-      title: 'Second server-side post', 
-      content: 'This is also coming from the server'
-    }
-  ]
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
   });
+});
+
+app.delete('/api/posts/:id', (req,res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Post deleted!' });
+  })
 });
 
 module.exports = app;
