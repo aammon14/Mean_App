bc2d5bb0e0e5804d1b116be4ec83b5ac113b7b42
Andrew Ammon <andrewammon@Andrews-MBP.home>
2019-01-28 21:07:28 -0500

    added mime type validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,61 +2,60 @@ import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 
-import { Post } from '../post.model';
 import { PostsService } from "../post.service";
-import { formGroupNameProvider } from "@angular/forms/src/directives/reactive_directives/form_group_name";
-
+import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent implements OnInit {
-  enteredContent = '';
-  enteredTitle = '';
+  enteredTitle = "";
+  enteredContent = "";
   post: Post;
   isLoading = false;
   form: FormGroup;
-  private mode = 'create';
+  imagePreview: string;
+  private mode = "create";
   private postId: string;
 
   constructor(
-    public postsService: PostsService, 
+    public postsService: PostsService,
     public route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      title:  new FormControl(null, {
+      title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      content: new FormControl(null, {
-        validators: [Validators.required]
-      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
       image: new FormControl(null, {
-        validators: [Validators.required]
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has('postId')) {
-        this.mode = 'edit';
-        this.postId = paramMap.get('postId');
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
           this.post = {
-            id: postData._id, 
-            title: postData.title, 
+            id: postData._id,
+            title: postData.title,
             content: postData.content
           };
           this.form.setValue({
-            'title': this.post.title, 
-            'content': this.post.content
+            title: this.post.title,
+            content: this.post.content
           });
         });
       } else {
-        this.mode = 'create';
+        this.mode = "create";
         this.postId = null;
       }
     });
@@ -64,21 +63,28 @@ export class PostCreateComponent implements OnInit {
 
   onImagePicked(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
-    this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form)
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
     if (this.form.invalid) {
-      return
+      return;
     }
     this.isLoading = true;
-    if (this.mode === 'create') {
+    if (this.mode === "create") {
       this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content)
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
     }
     this.form.reset();
   }
